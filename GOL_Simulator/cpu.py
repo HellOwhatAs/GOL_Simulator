f65db0c7a4238a41bb22be0097f70e3fba5bceb8
HellOwhatAs <xjq701229@outlook.com>
2023-07-26 13:56:18 +0800

    border

--- GOL_Simulator/cpu.py
+++ GOL_Simulator/cpu.py
@@ -1,35 +1,41 @@
 from numba import njit, prange
 import numpy as np
+import numpy.typing
 from typing import Union
 
 @njit(parallel=True)
-def update_new(world,new_world):
-    for i in prange(1,world.shape[0]-1):
-        for j in range(1,world.shape[1]-1):
-            tmp=(world[i-1,j-1])+\
-            (world[i-1,j])+\
-            (world[i-1,j+1])+\
-            (world[i,j-1])+\
-            (world[i,j+1])+\
-            (world[i+1,j-1])+\
-            (world[i+1,j])+\
-            (world[i+1,j+1])
+def update_new(world, new_world):
+    for i in prange(world.shape[0]):
+        for j in range(world.shape[1]):
+            tmp = (
+                (world[(i-1) % world.shape[0], (j-1) % world.shape[1]])+
+                (world[(i-1) % world.shape[0], (j  ) % world.shape[1]])+
+                (world[(i-1) % world.shape[0], (j+1) % world.shape[1]])+
+                (world[(i  ) % world.shape[0], (j-1) % world.shape[1]])+
+                (world[(i  ) % world.shape[0], (j+1) % world.shape[1]])+
+                (world[(i+1) % world.shape[0], (j-1) % world.shape[1]])+
+                (world[(i+1) % world.shape[0], (j  ) % world.shape[1]])+
+                (world[(i+1) % world.shape[0], (j+1) % world.shape[1]])
+            )
 
-            if tmp==3:new_world[i,j]=1
-            elif tmp<2 or tmp>3:new_world[i,j]=0
+            if tmp == 3: new_world[i,j]=1
+            elif tmp<2 or tmp>3: new_world[i,j]=0
 
 class Simulator:
-    def __init__(self,init_state:np.ndarray):
-        self.__field:np.ndarray=np.zeros(tuple(i+2 for i in init_state.shape),dtype=np.bool8)
-        self.__field[1:-1,1:-1]=init_state
-        self.__new_field=(self.__field).copy()
-    def run(self,rounds:int=1,tqdm=None):
+    def __init__(self, init_state: numpy.typing.NDArray):
+        self.__field = np.zeros(init_state.shape, dtype=np.bool8)
+        self.__field[:] = init_state
+        self.__new_field = (self.__field).copy()
+    
+    def run(self, rounds: int=1, tqdm = None):
         for _ in range(rounds) if tqdm is None else tqdm(range(rounds)):
-            update_new(self.__field,self.__new_field)
-            self.__field[:]=self.__new_field
+            update_new(self.__field, self.__new_field)
+            self.__field[:] = self.__new_field
         return self
-    def __getitem__(self,x)->Union[np.ndarray,np.bool_]:
-        return self.__field[1:-1,1:-1][x]
-    def __setitem__(self,__s,__o):
-        self.__field[1:-1,1:-1][__s]=__o
-        self.__new_field[1:-1,1:-1][__s]=__o
+    
+    def __getitem__(self, x) -> Union[numpy.typing.NDArray[np.uint8], np.bool8]:
+        return self.__field[x]
+    
+    def __setitem__(self, __s, __o):
+        self.__field[__s] = __o
+        self.__new_field[__s] = __o
